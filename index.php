2144f3896cca20163ca7ee8909b376c53f7f1556
unknown <leezacyberlink@gmail.com>
2023-05-24 16:11:08 +0545

    version updated

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
             <h2 class="secondary-font uk-margin-bottom-small" uk-scrollspy="cls: unimation-fade;  delay: 300; repeat: false ">You are in Safe Hands! </h2>
             <h2 class="primary-font uk-margin-remove-top"  uk-scrollspy="cls: uk-animation-fade;  delay: 300; repeat: false ">Your wellness is our mission.</h2>
             <p class="para-font"  uk-scrollspy="cls: uk-animation-fade;  delay: 300; repeat: false ">"Expert care with compassion. Your wellness is our top priority. Experience exceptional healthcare close to home. Committed to empowering your health and providing nurturing support.</p>
-            <a href="about.php" class="banner-button">Book Appointment</a>
+            <a href="about.php" class="banner-button ">Book Appointment</a>
             <a href="about.php" class="banner-button1">Contact Us</a>
             </div>
         </div>
@@ -72,13 +72,44 @@
 </section> -->
 <!-- Emergency section -->
 <section class="emergency-section">
-   <div class="uk-container uk-padding-large uk-padding-remove-top" >
+   <div class="uk-container uk-padding-large" >
       <ul class="uk-grid-collapse uk-child-width-expand@m" uk-grid>
-         <li style="background-color:#0057B7;">sdfs</li>
-         <li style="background-color:#A5DDEF;">sdfs</li>
-         <li style="background-color:#0057B7;">sdfsd</li>
+        <li style="margin:0 10px ;">
+          <div class="top-icon-section">
+            <img src="assets/img/icons/facility.png" alt="" height="80px;" width="80px;">
+          </div>
+          <div class="text-section uk-padding">
+          <p class="uk-margin-top">
+              <h1 class="small-primary-font">Endocrinology</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quas ipsa </p>
+          </p>
+          </div>
+        </li>
+        <li style="margin:0 10px ;">
+          <div class="top-icon-section">
+            <img src="assets/img/icons/facility.png" alt="" height="80px;" width="80px;">
+          </div>
+          <div class="text-section uk-padding">
+          <p class="uk-margin-top">
+              <h1 class="small-primary-font">Endocrinology</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quas ipsa </p>
+          </p>
+          </div>
+        </li>
+        <li style="margin:0 10px ;">
+          <div class="top-icon-section">
+            <img src="assets/img/icons/facility.png" alt="" height="80px;" width="80px;">
+          </div>
+          <div class="text-section uk-padding">
+          <p class="uk-margin-top">
+              <h1 class="small-primary-font">Endocrinology</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quas ipsa </p>
+          </p>
+          </div>
+        </li>
+       
       </ul>
-
+      </ul>
    </div>
 </section>
 <!-- About us Section -->
@@ -86,27 +117,38 @@
 <div id="about" class="uk-section  uk-section-primary" style="background-color: #526178;">
   <div class="uk-container">
     <div class="uk-child-width-1-2@m uk-grid-match uk-grid" data-uk-grid="">
-      <div class="uk-first-column ">
-        <div class="uk-padding-large uk-padding-remove-left uk-padding-remove-vertical uk-flex uk-flex-column">
-          <h2 class="uk-heading-small uk-margin-medium-top">About Us</h2>
-          <hr class="uk-margin-auto-top uk-separator-small">
-          <div class="uk-margin-auto-bottom">
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
-            nulla pariatur. Excepteur sint occaecat cupidatat non proident. </p>
-            <p>Duis aute irure dolor in quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo 
-            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore.</p>           
-          </div>
-          
-          <!-- <h3 class="uk-margin-remove uk-text-uppercase uk-h5 uk-letter-spacing-small"><a class="hvr-forward" href="#contact" data-uk-scroll="offset: 200">Contact<span class="uk-margin-left uk-icon" data-uk-icon="arrow-right"><svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" data-svg="arrow-right"><polyline fill="none" stroke="#000" points="10 5 15 9.5 10 14"></polyline><line fill="none" stroke="#000" x1="4" y1="9.5" x2="15" y2="9.5"></line></svg></span></a></h3> -->
-        </div>
-      </div> 
-      <div class="">
+    <div class="">
         <div class="uk-height-1-1 uk-background-center-center uk-background-cover uk-background-norepeat uk-background-blend-overlay 
           uk-overlay-retreat" style="background-image: url();">
           <img class="uk-box-shadow-large" src="https://thumbs.dreamstime.com/b/building-blue-letter-h-sign-hospital-large-modern-184908229.jpg" alt="About Us">
         </div>
-      </div>  
+      </div> 
+      <div class="uk-first-column ">
+        <div class=" uk-flex uk-flex-column">
+          <h1 class="secondary-font">About Hospital</h1>
+          <h1 class="primary-font uk-margin-remove">The South City Hospital</h1>
+          <hr>
+          <p class="para-font">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, accusantium illum? Laudantium ad maxime sed, delectus debitis doloribus similique libero dicta voluptates blanditiis in consequatur quod inventore sint nesciunt. A?</p>
+          <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body uk-padding-small ">Top professional Team</div>
+            </div>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body uk-padding-small">Multi Functional Hospital</div>
+            </div>
+          </div>
+          <div class="uk-grid-small uk-child-width-expand@s uk-text-center" uk-grid>
+            <div>
+                <div class="uk-card uk-card-default uk-card-body uk-padding-small">Top professional Team</div>
+            </div> 
+            <div>
+                <div class="uk-card uk-card-default uk-card-body uk-padding-small">Top professional Team</div>
+            </div>
+          </div>
+          
+        </div>
+      </div> 
+       
     </div>
   </div>
 </div>
