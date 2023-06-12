fa70f3ec65f894d07a2f850ada5148a0586b220b
unknown <leezacyberlink@gmail.com>
2023-06-12 14:50:10 +0545

    team

--- about.php
+++ about.php
@@ -6,32 +6,9 @@
         <p class="para-font uk-margin-remove" style="color:white;">Home < About</p>
     </div>
 </section>
-<!-- <section class="uk-section about-top-section">
-    <div class="uk-container">
-    <ul class="uk-grid-collapse uk-child-width-expand@m padding-large-top">
-        <li class="uk-padding-small"> <img class="uk-box-shadow-large" src="https://thumbs.dreamstime.com/b/building-blue-letter-h-sign-hospital-large-modern-184908229.jpg" alt="About Us" style="height:650pcb c  fbhx; object-fit:cover;"></li>
-        <li class="uk-padding-small" >
-            <ul class="uk-tab " uk-height-match="target: li>a" uk-tab="">
-                <li class="uk-active"><a href="#" aria-expanded="true">Introduction</a></li>
-                <li class="uk-margin-auto"><a href="#" aria-expanded="false" style="">Mission/Vision</a></li>
-                <li class="uk-margin-auto"><a href="#" aria-expanded="false" style="">Course Brochures</a></li>
-            </ul>
-            <ul class="uk-switcher">
-                <li class="uk-active">
-                <h1 class="green-secondary-font uk-margin-remove-bottom uk-margin-top">About Hospital</h1>
-                    <h1 class="section-primary-font uk-margin-remove">Medi Quest Laboratory Clinic</h1>
-                        <p class="para-font uk-text-justify">The concept of Mediquest Lab was envisioned a decade ago to fulfill the gap in molecular and other advanced diagnostic services for cancer patients in the country. The group of pathologists and hematologists urged the desperate need for flow cytometry, polymerized chain reaction (PCR), and cytogenetics services and decided to come up with a state-of-the-art diagnostic pathology center. <br><br>
-                        Over the years, Mediquest Laboratory has scaled new heights and expanded its wing towards general pathology services, again to provide accurate, detailed, and dependable reports for our clients. <br><br>
-                        We take pride in being the first commercial laboratory to pioneer immunohistochemistry (IHC), flow cytometry, polymerized chain reaction (PCR), and cytogenetics tests in the country. <br><br>
-                        At Mediquest Lab, we are determined to provide world-class, error-free pathology services in Nepal.</p>
-                </li>
-    </ul>
-    </div>
-</section> -->
 
 <section class="uk-section">
     <div class="uk-container">
-
        <ul class="uk-tab tab-silder" uk-height-match="target: li>a" uk-tab="" >
             <li class="uk-active uk-margin-auto para-font tabs "><a href="#" aria-expanded="true">Introduction</a></li>
             <li class="uk-margin-auto para-font tabs"><a href="#" aria-expanded="false" style="">Mission/Vision</a></li>
@@ -39,7 +16,7 @@
         </ul>
  
         <ul class="uk-switcher">
-            <li class="uk-active">
+            <li class="uk-active uk-padding-small">
                 <ul class="uk-grid-collapse uk-child-width-expand@m" uk-grid>
                     <li class="uk-margin-bottom">
                         <div>
@@ -49,7 +26,7 @@
                             </div>
                         </div> 
                     </li>
-                    <li style="padding:0px 20px;">
+                    <li class="uk-padding-small">
                         <div class=" uk-flex uk-flex-column " >
                             <h1 class="green-secondary-font uk-margin-remove-bottom ">About Hospital</h1>
                             <h1 class="section-primary-font uk-margin-remove ">Medi Quest Laboratory Clinic</h1>
@@ -62,7 +39,7 @@
             </li>
             <li>
                <ul class="uk-grid-collapse uk-child-width-expand@m" uk-grid >
-               <li class="uk-margin-bottom">
+               <li class="uk-margin-bottom uk-padding-small">
                         <div>
                             <div class="uk-height-1-1 uk-background-center-center uk-background-cover uk-background-norepeat uk-background-blend-overlay 
                             uk-overlay-retreat" >
@@ -70,7 +47,7 @@
                             </div>
                         </div> 
                 </li>
-                <li>
+                <li class="uk-padding-small">
                     <h1 class="green-secondary-font uk-margin-remove-bottom"> Misson/Values</h1>
                     <h1 class="section-primary-font uk-margin-remove">Learn About our Mision</h1>
                     <hr>
@@ -89,80 +66,39 @@
     </div>
     
 </section>
-<section class="uk-section team-section " >
-    <div class="uk-container">
-        <div class="uk-text-center uk-margin-large-bottom">
-        <h1 class="blue-secondary-font uk-margin-remove-bottom">Team members</h1>
-        <h1 class="primary-font uk-margin-small">Meet our Staff Members</h1>
-    </div>
-    <div class="uk-padding uk-padding-remove-top">
-        <div class="uk-slider-container-offset" uk-slider>
-            <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
-                <div>
-                    <ul class="uk-slider-items uk-child-width-1-3@s uk-grid">
-                            <li>
-                                <div class=" uk-padding-small">
-                                    <div class="uk-card uk-card-default uk-team-member" >
-                                        <div class="uk-card-media-top">
-                                            <img src="https://trister.com/wp-content/uploads/2022/05/team4.jpg" width="1800" height="1200" alt="">
-                                        </div>  
-                                        <div class="uk-card-body uk-text-center uk-padding-small" >
-                                            <h3 class="services-font uk-margin-remove">Dr Amanda Cyrus</h3>
-                                            <p class="para-font uk-margin-remove">Orthoprdic Surgeon</p>
-                                            <div>
-                                                <a href="" class="uk-icon-button uk-margin-small-right" uk-icon="twitter"></a>
-                                                <a href="" class="uk-icon-button  uk-margin-small-right" uk-icon="facebook"></a>
-                                                <span uk-icon="icon: check; ratio: 2"></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> 
-                            </li>
-                            <li>
-                                <div class=" uk-padding-small">
-                                    <div class="uk-card uk-card-default uk-team-member" >
-                                        <div class="uk-card-media-top">
-                                            <img src="https://trister.com/wp-content/uploads/2022/05/team4.jpg" width="1800" height="1200" alt="">
-                                        </div>  
-                                        <div class="uk-card-body uk-text-center uk-padding-small" >
-                                            <h3 class="services-font uk-margin-remove">Dr Amanda Cyrus</h3>
-                                            <p class="para-font uk-margin-remove">Orthoprdic Surgeon</p>
-                                            <div>
-                                                <a href="" class="uk-icon-button uk-margin-small-right" uk-icon="twitter"></a>
-                                                <a href="" class="uk-icon-button  uk-margin-small-right" uk-icon="facebook"></a>
-                                                <span uk-icon="icon: check; ratio: 2"></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> 
-                            </li>
-                            <li>
-                                <div class=" uk-padding-small">
-                                    <div class="uk-card uk-card-default uk-team-member" >
-                                        <div class="uk-card-media-top">
-                                            <img src="https://trister.com/wp-content/uploads/2022/05/team4.jpg" width="1800" height="1200" alt="">
-                                        </div>  
-                                        <div class="uk-card-body uk-text-center uk-padding-small" >
-                                            <h3 class="services-font uk-margin-remove">Dr Amanda Cyrus</h3>
-                                            <p class="para-font uk-margin-remove">Orthoprdic Surgeon</p>
-                                            <div>
-                                                <a href="" class="uk-icon-button uk-margin-small-right" uk-icon="twitter"></a>
-                                                <a href="" class="uk-icon-button  uk-margin-small-right" uk-icon="facebook"></a>
-                                                <span uk-icon="icon: check; ratio: 2"></span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div> 
-                            </li>
-                    </ul>
-                </div>
-                    <a class="uk-position-center-left uk-position-small left-arrow" href="#" uk-slidenav-previous uk-slider-item="previous" ></a>
-                    <a class="uk-position-center-right uk-position-small  right-arrow" href="#" uk-slidenav-next uk-slider-item="next"></a> 
-            </div>
-            
-            <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin uk-hidden"></ul>
+<section class="uk-section team-section uk-padding-remove-top" >
+    <div class="uk-container ">
+        <div class="uk-text-center ">
+            <h2 class="green-secondary-font uk-margin-remove-bottom ">Team Members</h2>
+            <h1 class="section-primary-font uk-margin-remove">Meet our Team Members</h1>
         </div>
-  </div>
+        <ul class="uk-grid-collapse uk-child-width-1-4@s uk-child-width-1-1" uk-grid>
+            <li class="uk-padding-small">
+                <div class="team-img ">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2bLci2M7BopkrG-lguOOBCBASxECMpsL-Hg&usqp=CAU" alt="">
+                </div>
+                <div class="team-text">
+                    <h4 class="uk-margin-small-top uk-margin-remove">Dr.Sampurna Tuladhar</h4>
+                    <p class="uk-margin-remove">Pathologists</p>
+                </div>
+            </li>
+            <li class="uk-padding-small">
+                <div class="team-img ">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2bLci2M7BopkrG-lguOOBCBASxECMpsL-Hg&usqp=CAU" alt="">
+                </div>
+            </li>
+            <li class="uk-padding-small">
+                <div class="team-img ">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2bLci2M7BopkrG-lguOOBCBASxECMpsL-Hg&usqp=CAU" alt="">
+                </div>
+            </li>
+            <li class="uk-padding-small">
+                <div class="team-img ">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2bLci2M7BopkrG-lguOOBCBASxECMpsL-Hg&usqp=CAU" alt="">
+                </div>
+            </li>
+        </ul>
+    </div>
 </section >
 
 <?php include('assets/include/footer.php');?>
