703187e87f0c7b283f85be056ad720ab072a478b
Leeza Shakya <leezashaky12@gmail.com>
2023-06-14 09:04:58 +0545

    change to inner-team

--- inner-team.php
+++ inner-team.php
@@ -7,5 +7,48 @@
         <p class="para-font  uk-text-center" style="color:white;">Home < Pathalogist <  <span class="active-page">Dr.Sampurna Tuladhar</span> </p>
     </div>
 </section>
+<section class="uk-section">
+    <div class="uk-container">
+        <ul class="uk-grid-collapse uk-child-width-1-2@s uk-child-width-2-2@m uk-child-width-1-1 uk-margin-top" uk-grid>
+            <li class="uk-padding-small uk-flex" style="background-color:red;">
+                <div class="team-img uk-transition-toggle uk-inline-clip">
+                    <img src="https://health.templines.info/wp-content/uploads/2017/09/doc4.jpg" alt="">
+                    <div class="uk-transition-slide-bottom uk-position-bottom uk-overlay uk-overlay-default  uk-text-center team-overlay">
+                        <p class="uk-h4 uk-margin-remove team-icon">
+                            <span uk-icon="icon: facebook"></span>
+                            <span uk-icon="icon: twitter"></span>
+                            <span uk-icon="icon:  linkedin"></span>
+                            <span uk-icon="icon: instagram"></span>
+                        </p>
+                    </div>
+                </div>
+                <div>
+                <p>DR Sampurna Tuladhar</p>
+                <p>vnlsikfvns vjosjvosjdvs vs</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, illo quidem iure autem, vero delectus ad iste placeat nemo quas veniam unde ut similique ea. Laudantium voluptate consectetur accusantium eius.</p>
+                </div>
+            </li>
+            <li class="uk-padding-small uk-flex" style="background-color:blue;" >
+                <div class="team-img uk-transition-toggle uk-inline-clip">
+                    <img src="https://health.templines.info/wp-content/uploads/2017/09/doc4.jpg" alt="">
+                    <div class="uk-transition-slide-bottom uk-position-bottom uk-overlay uk-overlay-default  uk-text-center team-overlay">
+                        <p class="uk-h4 uk-margin-remove team-icon">
+                            <span uk-icon="icon: facebook"></span>
+                            <span uk-icon="icon: twitter"></span>
+                            <span uk-icon="icon:  linkedin"></span>
+                            <span uk-icon="icon: instagram"></span>
+                        </p>
+                    </div>
+                </div>
+                <div>
+                <p>DR Sampurna Tuladhar</p>
+                <p>vnlsikfvns vjosjvosjdvs vs</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, illo quidem iure autem, vero delectus ad iste placeat nemo quas veniam unde ut similique ea. Laudantium voluptate consectetur accusantium eius.</p>
+                </div>
+            </li>
+                
+        </ul>
+    </div>
+</section>
 
 <?php include('assets/include/footer.php');?>
